0baa4daeae80addca478fedec8c22dc7e01ef03a
caiyining23 <cai.yining23@gmail.com>
2024-11-17 11:14:47 +0800

    Add files via upload

---
+++ exercise 9/src/CalculatorLogic.java
@@ -0,0 +1,28 @@
+public class CalculatorLogic {
+
+    public double performOperation(double num1, double num2, String operator) {
+        double result = 0;
+
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) {
+                    throw new ArithmeticException("Cannot divide by zero");
+                }
+                result = num1 / num2;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid operator");
+        }
+
+        return result;
+    }
+}
